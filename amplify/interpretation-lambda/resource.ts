28db8cc4ca259b4c4ffbe0d12bc514b77091081f
Nicholas Basinger <nicholasbasinger@Nicks-Air.lan>
2024-12-09 09:42:03 -0500

    updates

--- amplify/interpretation-lambda/resource.ts
+++ amplify/interpretation-lambda/resource.ts
@@ -3,4 +3,5 @@ import { defineFunction } from "@aws-amplify/backend";
 export const getInterpretation = defineFunction({
   name: "interpretation-lambda",
   entry: "./handler.ts",
+  timeoutSeconds: 30
 });
